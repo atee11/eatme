cd553886a16dfa09acd0818dc7bb57bf0472d90d
atee11 <toman.ati@gmail.com>
2024-03-12 19:40:05 +0100

    add carousel

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    subject: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+  });
 
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
 }
